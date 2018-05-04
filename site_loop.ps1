3dd8b1777de98cb252c13471736c88a026a9cd96
jcrocke9 <joseph.crockett@alexandriava.gov>
2018-05-04 11:57:23 -0400

    full loop capability for all site templates

--- site_loop.ps1
+++ site_loop.ps1
@@ -1,9 +1,38 @@
-Get-SPOSite -Limit ALL | Where-Object {$_.Template -eq "GROUP#0"} | ForEach-Object {
-    Connect-PnPOnline -Url $_
-    $Look = (Get-PnPTheme -DetectCurrentComposedLook).Name
+$creds = Get-Credential
+$LoginName = "joseph.crockett.a@alexandriava.gov"
+$adminUrl = "https://alexandriava1-admin.sharepoint.com"
+# Connect-SPOService -Url $adminUrl -Credential $creds
+# $TemplateWorkingOnNow = "STS#0"
+$TemplateWorkingOnNow = "GROUP#0"
+
+function Get-CoaSpoStsTheme () {
+    $Name = (Get-PnPTheme -DetectCurrentComposedLook).Name
+    $MasterPage = (Get-PnPTheme -DetectCurrentComposedLook).MasterPage
+    $Theme = (Get-PnPTheme -DetectCurrentComposedLook).Theme
     $CompDetails = New-Object psobject -Property @{
-        Url          = $_
-        ComposedLook = $Look
+        Url        = $Url
+        Name       = $Name
+        MasterPage = $MasterPage
+        Theme      = $Theme
     }
-    $CompDetails
+    $CompDetails | Export-Csv -Path C:\alex\out\siteDesign_sts_composedLook_tst.csv -Append -NoClobber -NoTypeInformation
+    Clear-Variable Name
+    Clear-Variable MasterPage
+    Clear-Variable Theme
+    Clear-Variable Url
+}
+
+function Get-CoaSpoGroupTheme () {
+    Get-PnPTheme
+}
+
+Get-SPOSite -Limit ALL | Where-Object {$_.Template -eq $TemplateWorkingOnNow} | ForEach-Object {
+    $Url = $_.Url
+    $Url
+    $UserTrue = Set-SPOUser -Site $Url -LoginName $LoginName -IsSiteCollectionAdmin $true
+    Start-Sleep -Seconds 1
+    Connect-PnPOnline -Url $Url -Credentials $creds
+    if ($TemplateWorkingOnNow = "STS#0") {Get-CoaSpoStsTheme}
+    if ($TemplateWorkingOnNow = "GROUP#0") {Get-CoaSpoGroupTheme}
+    $userFalse = Set-SPOUser -Site $Url -LoginName $LoginName -IsSiteCollectionAdmin $false
 }
