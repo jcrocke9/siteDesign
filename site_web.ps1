c884dcd4cd199ee821b455863d8c0d59780325f1
Joe Crockett <jcrocket@gmail.com>
2018-05-07 21:36:53 -0400

    loop for subwebs, and work on prod sites

--- site_web.ps1
+++ site_web.ps1
@@ -4,4 +4,7 @@ $creds = Get-Credential
 
 # Get-SPOSite -Identity "https://alexandriava1.sharepoint.com/sites/its"
 Connect-PnPOnline -Url "https://alexandriava1.sharepoint.com/sites/demo" -Credentials $creds
-Measure-PnPWeb -Recursive
+Get-PnPSubWebs -Recurse | ForEach-Object {
+    # $RelativeUrl = $_.ServerRelativeUrl
+    Get-PnPWeb -Identity $_
+}
--- sts_pnp_siteTheme.ps1
+++ sts_pnp_siteTheme.ps1
@@ -1,21 +1,32 @@
 #$creds = Get-Credential
 $LoginName = "joseph.crockett.a@alexandriava.gov"
 $adminUrl = "https://alexandriava1-admin.sharepoint.com"
-$Url = "https://alexandriava1.sharepoint.com/sites/pwa"
-#Connect-SPOService -Url $adminUrl -Credential $creds
+$Url = "https://alexandriava1.sharepoint.com/sites/demo"
+$BaseUrl = "https://alexandriava1.sharepoint.com"
+Connect-SPOService -Url $adminUrl -Credential $creds
 
-function Set-CoaPnpTheme ()
+function Set-CoaPnpTheme ([psobject]$SubWebConnection)
 {
-    Add-PnPFile -Path .\fontscheme000_Alex.spfont -Folder "_catalogs/theme/15"
-    Add-PnPFile -Path .\Palette000_Alex.spcolor -Folder "_catalogs/theme/15"
-    $siteRelUrl = (Get-PnPWeb).ServerRelativeUrl
+    Add-PnPFile -Connection $SubWebConnection -Path .\fontscheme000_Alex.spfont -Folder "_catalogs/theme/15"
+    Add-PnPFile -Connection $SubWebConnection -Path .\Palette000_Alex.spcolor -Folder "_catalogs/theme/15"
+    Add-PnPFile -Connection $SubWebConnection -Path .\CitySeal.png -Folder "SiteAssets"
+    $siteRelUrl = (Get-PnPWeb -Connection $SubWebConnection).ServerRelativeUrl
     $colorPaletteUrl = $siteRelUrl + "/_catalogs/theme/15/palette000_Alex.spcolor"
     $fontSchemeUrl = $siteRelUrl + "/_catalogs/theme/15/fontscheme000_Alex.spfont"
-    Set-PnPTheme -ColorPaletteUrl $colorPaletteUrl -FontSchemeUrl $fontSchemeUrl
+    $siteLogoUrl = $siteRelUrl + "/SiteAssets/CitySeal.png"
+    Set-PnPTheme -ColorPaletteUrl $colorPaletteUrl -FontSchemeUrl $fontSchemeUrl -Connection $SubWebConnection -ResetSubwebsToInherit
+    Set-PnPWeb -SiteLogoUrl $siteLogoUrl
+    Clear-Variable siteRelUrl
 }
 
 $UserTrue = Set-SPOUser -Site $Url -LoginName $LoginName -IsSiteCollectionAdmin $true
 Start-Sleep -Seconds 1
-Connect-PnPOnline -Url $Url -Credentials $creds
-Set-CoaPnpTheme
+$Connection = Connect-PnPOnline -Url $Url -Credentials $creds -ReturnConnection
+Set-CoaPnpTheme ($Connection)
+<# Get-PnPSubWebs -Recurse -Connection $Connection | ForEach-Object {
+    $CurrentSubWeb = $BaseUrl + $_.ServerRelativeUrl
+    $CurrentSubWeb
+    $SubWebConnection = Connect-PnPOnline -Url $CurrentSubWeb -Credentials $creds -ReturnConnection
+    Set-CoaPnpTheme ($SubWebConnection)
+} #>
 $userFalse = Set-SPOUser -Site $Url -LoginName $LoginName -IsSiteCollectionAdmin $false
